765160484555cce5edf607ea2dcecb99a3ac8c38
Pal Sipos <sipos.pal92@gmail.com>
2018-02-05 15:37:52 +0100

    osgDemo

--- visualizer/osgdemo/tiledownloader.sh
+++ visualizer/osgdemo/tiledownloader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osgearth_package --tms mymap_source.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 19 --out mymap_osm --out-earth newmymaposm.earth
+osgearth_package --tms mymap_source.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 15 --out mymap_v0205 --out-earth mymap_v0205.earth
